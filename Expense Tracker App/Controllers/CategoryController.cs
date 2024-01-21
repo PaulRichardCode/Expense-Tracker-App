b35f8d5e1edb0943b000829a2327a609b09ef828
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2024-01-21 19:00:30 +0100

    get set

---
+++ Expense Tracker App/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Expense_Tracker_App.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
